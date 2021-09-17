2a959ca2c0aaf8e5d57189d403041069c3b4ceb6
Carlos Garcia <cgmilan@gmail.com>
2021-09-16 23:41:41 -0500

    react router and layouts

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
 import React from "react";
-import Header from "./components/Header";
+import { Route, Switch } from "react-router-dom";
+
+import MainLayout from "./layouts/MainLayout";
+import HomepageLayout from "./layouts/HomepageLayout";
+
 import Homepage from "./pages/Homepage";
+import Registration from "./pages/Registration";
 import "./global.scss";
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <div className="main">
-        <Homepage />
-      </div>
+      <Switch>
+        <Route
+          exact
+          path="/"
+          render={() => (
+            <HomepageLayout>
+              <Homepage />
+            </HomepageLayout>
+          )}
+        />
+        <Route
+          path="/registration"
+          render={() => (
+            <MainLayout>
+              <Registration />
+            </MainLayout>
+          )}
+        />
+      </Switch>
     </div>
   );
 }
